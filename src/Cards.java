68dc3478e38d64cb576e2c6db1202c707f3f853d
caleb <calebh1@umbc.edu>
2021-12-06 20:38:33 -0500

    created a deck of cards

--- src/Cards.java
+++ src/Cards.java
@@ -1,12 +1,28 @@
 public class Cards {
     private int rank; //card variable
     private int suit; //card variable
-    private static String[] ranks = {"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"};
-    private static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    final int numCards = 52;
+    private static final String[] ranks = {"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"};
+    private static final String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
     //Creates cards (uses this keyword)
-    Cards(int suit, int rank) {
-        this.suit=suit;
-        this.rank=rank;
+    Cards(int rank, int suit) {
+       Cards[] deck = new Cards[numCards];
+       int index = 0;
+       for (suit = 0; suit <=3; suit++){
+           for(rank =1; rank <= 13; rank++){
+               deck[index] = new Cards(rank, suit);
+               index++;
+           }
+       }
+        for(int i = 0; i < deck.length; i++){
+            System.out.println(deck[i]);
+
+        }
     }
+    /*
+    //create deck using random
+    public cardDeck(){
+        deckofCards = new Cards[numCards];
+    }*/
 }
